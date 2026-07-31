5d937213780d68d5f17012bf32979a1eff559bba
awais9845 <waqarkhattak844@gmail.com>
2026-07-31 05:51:12 -0700

    changed dynamic data

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -730,7 +730,7 @@ export default function Dashboard() {
                     <div className="text-right">
                       <span className="block">AUTHORIZED SIGNATURE:</span>
                       <span className="text-gold-500 font-serif font-semibold italic text-[10px]">
-                        Hafiz Huzaifa Shah
+                        Syed Hammad Hassan
                       </span>
                     </div>
                   </div>
